5c578e3b3ddeab44fc268d1078e2f861ca1e4972
Mackain <markus.j.lindgren@gmail.com>
2023-04-10 17:30:08 +0200

    validator WIP

---
+++ infinitum.core/utils/Validator.cs
@@ -0,0 +1,25 @@
+namespace infinitum.core.Utils;
+using System.Security.Cryptography;
+
+
+public class Validator
+{
+    public bool ValidateBlock(Block b)
+    {
+        return true;
+    }
+
+    public bool ValidateBlockchain(List<Block> c)
+    {
+        return true;
+    }
+
+    public bool ValidatePublicKey(string privateKey, string publicKey)
+    {
+        var sha = SHA256.Create();
+        byte[] hash = sha.ComputeHash(privateKey);
+        string str = System.Text.Encoding.UTF8.GetString(hash, 0, hash.Length);
+        return String.Equals(publicKey, str);
+    }
+    
+}
